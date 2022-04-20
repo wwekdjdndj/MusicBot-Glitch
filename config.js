fc383854994d4799bb498969a70340c48551f676
wwekdjdndj <101439099+wwekdjdndj@users.noreply.github.com>
2022-04-20 14:27:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
         DJ: {
             enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
+            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume ,search'] //Please don't touch
         },
         maxVol: 250, //You can specify the maximum volume level.
         loopMessage: false, //Please don't touch
